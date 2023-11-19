cae4352beb2803ba7e2ab3b8c8a8d3000b800f09
Waresh-bng <warashhasan@gmail.com>
2023-11-19 22:46:07 +0600

    final commit

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index(Request $request)
+    {
+
+        $name = "Donal Trump";
+        $age = "75";
+        $id= $request->id;
+
+        $data = [
+            "id" => $id,
+            "name" => $name,
+            "age" => $age,
+        ];
+
+        $cookieName = 'access_token';
+        $value = '123-XYZ';
+        $minute = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        $cookie = cookie(
+            $cookieName,
+            $value,
+            $minute,
+            $path,
+            $domain,
+            $secure,
+            $httpOnly
+        );
+        return response($data, 200)->cookie($cookie);
+    }
+}
